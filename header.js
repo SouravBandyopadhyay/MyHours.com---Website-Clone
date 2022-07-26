f4ee4e42c8a75d1082f0ab0a85a4a823200652bf
Sourav Bandyopadhyay <103631425+SouravBandyopadhyay@users.noreply.github.com>
2022-07-26 21:06:42 +0530

    Final Push

---
+++ header.js
@@ -0,0 +1,17 @@
+let navbar = () => {
+  return `<div id="navbar">
+    <img
+      id="navlogo"
+      src="https://uploads-ssl.webflow.com/5c77a918ef19681741be7bca/5fd37c83dfa3ccb0d2d9836f_myhours-logo.svg"
+      alt=""/>
+    <div id="navpages">
+      <p class="navlink"><a class="nav-a" href="./index.html">How It Works</a></p>
+      <p class="navlink"><a class="nav-a" href="./use.html">Use Cases</a></p>
+      <p class="navlink"><a class="nav-a" href="./price.html">Pricing</a></p>
+      <p class="navlink"><a class="nav-a" href="./supportpage.html">Support</a></p>
+      <p class="navlink"><a class="nav-a"href="./login.html">Sign in</a></p>
+      <button id="navbtn1"> <a  href="track.html">Get My Hours Free</a></button>
+    </div>
+  </div>`;
+};
+export default navbar;
